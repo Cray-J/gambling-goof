2d16c30719269898671a69847f4a014250c260cc
Kristoffer Jensen <kje@computas.com>
2018-04-01 00:53:40 +0200

    Added prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDz7surFmHfXQuftIm62k-28vkNI7oEn0M',
+    authDomain: 'gambling-goof-prod.firebaseapp.com',
+    databaseURL: 'https://gambling-goof-prod.firebaseio.com',
+    projectId: 'gambling-goof-prod',
+    storageBucket: 'gambling-goof-prod.appspot.com',
+    messagingSenderId: '483023253127'
+  }
 };
